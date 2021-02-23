e0b4fcf5b50e345edfb47c2a5b508e41a84a5b93
Prasanth Sengadu Suresh <prasuchit@gmail.com>
2021-02-23 18:54:14 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -7,16 +7,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Website under construction!
+          Website under construction!
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
